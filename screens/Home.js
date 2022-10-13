8a82299a31de5093e5abaee70203aee7f3429b28
Zarivate <Zarivate1@gmail.com>
2022-10-12 23:56:36 -0500

    Trying to get heroIcons to work for Home component

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,13 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
 
 const Home = () => {
   // Gives access to navigation object
@@ -15,16 +22,22 @@ const Home = () => {
   }, []);
 
   return (
-    // No matter phone type, SafeAreaView, will ensure it doesn't go to far above or below user's screen
     <SafeAreaView>
-      <Text className="text-blue-500">
-        <View>
+      <Text className="text-red-500">
+        {/* Header goes here */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
-              uri: "https://reactnative.dev/img/tiny_logo.png",
+              uri: "https://links.papareact.com/wru",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
         </View>
       </Text>
     </SafeAreaView>
