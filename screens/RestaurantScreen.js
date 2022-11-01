9988d00a7788bf61b14127a4c29c0e66e4db8cf0
Zarivate <Zarivate1@gmail.com>
2022-10-31 23:58:40 -0500

    Got logic and design of dishes working, a package wasn't able to be installed correctly however

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../server/sanity";
+import DishRow from "../components/DishRow";
 import {
   ArrowLeftIcon,
   ChevronRightIcon,
@@ -80,6 +81,16 @@ const RestaurantScreen = () => {
       <View>
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
         {/* Dishes component goes here */}
+        {dishes.map((dish) => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </ScrollView>
   );
