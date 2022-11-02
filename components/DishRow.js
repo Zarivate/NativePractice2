e8ee57b6e8d1f9b5feebb24dc1d7a22e5f4d9282
Zarivate <Zarivate1@gmail.com>
2022-11-01 23:59:13 -0500

    Trying to finalize design of dishes

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,12 +1,29 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import Currency from "react-currency-formatter";
 import React from "react";
+import { urlFor } from "../server/sanity";
 
 const DishRow = ({ id, name, description, price, image }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className="bg-white border p-4 border-gray-200">
+      <View className="flex-row">
+        <View>
+          <Text className="text-lg mb-1">{name}</Text>
+          <Text className="text-gray-400">{description}</Text>
+          <Text className="text-gray-400 mt-2">
+            <Currency quantity={price} currency="USD" />
+          </Text>
+        </View>
+      </View>
       <View>
-        <Text className="text-lg mb-1">{name}</Text>
-        <Text className="text-gray-400">{description}</Text>
+        <Image
+          style={{
+            borderWidth: 1,
+            borderColor: "#F3F3F4",
+          }}
+          source={{ uri: urlFor(image).url() }}
+          className="h-20 w-20 bg-gray-300 p-4"
+        />
       </View>
     </TouchableOpacity>
   );
