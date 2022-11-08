217a84939cf940ead5c5f4670e6454bc8f27d931
Zarivate <Zarivate1@gmail.com>
2022-11-07 23:56:17 -0600

    Added animated loading screen and implemented Google Maps for restaurant location

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import BasketScreen from "./screens/BasketScreen";
 import LoadingOrderScreen from "./screens/LoadingOrderScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +25,11 @@ export default function App() {
           <Stack.Screen
             name="LoadingOrder"
             component={LoadingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }}
+          />
+          <Stack.Screen
+            name="Delivery"
+            component={DeliveryScreen}
             options={{ presentation: "fullScreenModal", headerShown: false }}
           />
         </Stack.Navigator>
--- screens/LoadingOrderScreen.js
+++ screens/LoadingOrderScreen.js
@@ -1,9 +1,20 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import * as Animatable from "react-native-animatable";
 import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
 
 const LoadingOrderScreen = () => {
+  // Since there isn't an actual restaurant to accept the orders, it's simulated using a useEffect after some time
+  const navigation = useNavigation();
+
+  // After a few seconds the screen is navigated back to the final delivery screen
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Delivery");
+    }, 4000);
+  }, []);
+
   return (
     <SafeAreaView className="bg-white flex-1 justify-center items-center">
       <Animatable.Image
@@ -19,7 +30,7 @@ const LoadingOrderScreen = () => {
       >
         Waiting on restaurant to accept order
       </Animatable.Text>
-      <Progress.Circle size={60} indeterminate={true} color="yellow" />
+      <Progress.CircleSnail size={50} color="black" />
     </SafeAreaView>
   );
 };
